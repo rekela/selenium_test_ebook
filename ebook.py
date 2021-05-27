f412d3b8bceadcdab8eed6db7462d48bb3e7f11e
rekela <ela.kawczak@gmail.com>
2021-05-27 05:20:51 +0200

    ebooks list updated

--- ebook.py
+++ ebook.py
@@ -5,18 +5,28 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.action_chains import ActionChains
 
 
-ebooks = (
-        'data-ethics-preference-management',
-        'cdp-vs-crm',
-        'ai-product-recommendation-engines',
-        'cookbook-of-marketing-automation-in-ecommerce',
-        'definitive-guide-to-email-deliverability',
-        'complete-guide-to-ma-in-ecommerce',
-        'online_consumer_trends',
-        'how_marketing_automation_is_transformed_by_ai_and_data_science',
-        'ebook_live_chat',
-        '10_success_stories_ebook'
-        )
+ebooks = {
+    "Data Ethics & Customer Preference Management":
+        "data-ethics-preference-management",
+    "The Ultimate marketer's guide to Customer Data Platforms":
+        "cdp-vs-crm",
+    "AI Product Recommendation Engines":
+        "ai-product-recommendation-engines",
+    "Cookbook of marketing automation in eCommerce":
+        "cookbook-of-marketing-automation-in-ecommerce",
+    "Kamasutra of eMail Marketing Deliverability":
+        "definitive-guide-to-email-deliverability",
+    "The Complete Guide to Marketing Automationin Ecommerce":
+        "complete-guide-to-ma-in-ecommerce",
+    "Online Consumer Trends 2020":
+        "online_consumer_trends",
+    "How Marketing Automation is transformed by AI and Data Science":
+        "how_marketing_automation_is_transformed_by_ai_and_data_science",
+    "How Live Chat is redefined by AI, Machine Learning, Deep Learning and Bots":
+        "ebook_live_chat",
+    "10 success stories of Europe's biggest brands implementing marketing automation":
+        "10_success_stories_ebook"
+}
 
 
 class Ebook(unittest.TestCase):
@@ -27,7 +37,7 @@ class Ebook(unittest.TestCase):
         self.options.add_argument("--disable-notifications")
         self.options.add_argument("start-maximized")
 
-        self.browser = webdriver.Chrome(chrome_options=self.options)
+        self.browser = webdriver.Chrome(options=self.options)
 
     def tearDown(self):
         self.browser.close()
@@ -54,7 +64,7 @@ def test_generator(ebook_name):
 
         email = self.browser.find_element_by_id("email")
         action.move_to_element(email).click().perform()
-        email.send_keys("ann@gla.com")
+        email.send_keys("ann@gmail.com")
 
         self.browser.find_element_by_name("company").click()
         self.browser.find_element_by_name("company").send_keys("GLA")
@@ -65,16 +75,21 @@ def test_generator(ebook_name):
 
         phone_number = self.browser.find_element_by_id("phoneNumber")
         action.move_to_element(phone_number).click().perform()
-        phone_number.send_keys("723434885")
+        phone_number.send_keys("123456789")
 
-        submit_button = self.browser.find_element_by_xpath("//button[@type='submit']")
+        self.assertTrue(self.browser.find_element_by_xpath("//div[@class='thanks-message']"))
+
+        submit_button = self.browser.find_element_by_xpath("//*[@id='uspForm']/div[2]/div/button")
+        action.move_to_element(submit_button).click(submit_button).perform()
         submit_button.submit()
+
+        self.assertTrue(self.browser.find_element_by_xpath("//div[@class='thanks-message']"))
 
     return testEbookDownload
 
 
 if __name__ == '__main__':
-    tested_ebook = ebooks[6]
+    tested_ebook = ebooks["Online Consumer Trends 2020"]
     test_name = 'test {}'.format(tested_ebook)
     test = test_generator(tested_ebook)
     setattr(Ebook, test_name, test)
